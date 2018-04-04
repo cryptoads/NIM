a504bc2c1b67973bbc697a3833395a38023194ae
CMichels <chrisjohnmichels@gmail.com>
2018-04-04 09:25:41 -0400

    Implemented computer player 2 that always wins

--- nim.js
+++ nim.js
@@ -1,5 +1,6 @@
 var player = 'player 1';
 var winner = false;
+var playerChoice = 0;
 rocks = ['@','@','@','@','@','@','@','@','@','@','@','@','@','@','@','@'];
 
 function printRocks() {
@@ -20,23 +21,36 @@ function checkWinner(rocks){
 
 }
 
+function roboPlay(){
+  alert(player + " has chosen "+ (4-Number(playerChoice)) + " rocks")
+  for(var i=0; i < (4 - Number(playerChoice)); i++ ){
+    rocks.pop();
+  }
+
+  player = 'player 1';
+}
+
 function pickRocks(){
+   if (player === 'player 1'){
     alert(player + "'s turn. There are "+ rocks.length+" left.");
     var numRocks = prompt("Number Of rocks? 1-3");
         if(numRocks <= rocks.length && numRocks <= 3 && numRocks >= 1){
           for(var i = 0; i < numRocks; i++){
               rocks.pop();
-              if(player === 'player 1'){
-              player = 'player 2';
-              }else{
-              player='player 1';
-            }}
+            }
+            playerChoice = numRocks;
+            player = 'player 2';
     }else{
         alert('bad selection');
     }
+
     checkWinner(rocks);
     printRocks();
+}else{
+  roboPlay();
 }
+}
+
 
 while(winner === false){
     pickRocks();
