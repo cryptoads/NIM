f6ed8bc897075f166071ba6e60920fac3df71a55
CMichels <chrisjohnmichels@gmail.com>
2018-04-04 09:03:14 -0400

    Update nim.js

--- nim.js
+++ nim.js
@@ -1,12 +1,10 @@
 var player = 'player 1';
 var winner = false;
-rocks = [['@','@','@','@'],['@','@','@','@'],['@','@','@','@'],['@','@','@','@']];
+rocks = ['@','@','@','@','@','@','@','@','@','@','@','@','@','@','@','@'];
 
 function printRocks() {
-    console.log(rocks[0]);
-    console.log(rocks[1]);
-    console.log(rocks[2]);
-    console.log(rocks[3]);
+    console.log(rocks);
+    console.log('**************');
 }
 
 function checkPile(pile){
@@ -14,29 +12,25 @@ function checkPile(pile){
 }
 
 function checkWinner(rocks){
- if(rocks.join(',').replace(/\,/g, "").length <= 3){
-    alert(player + 'wins');
+
+ if(rocks.length <= 3 ){
+    alert(player + ' wins');
     winner = true;
- }
+    }
+
 }
 
 function pickRocks(){
-    alert(player + "'s turn");
-    var pilePicked = prompt("Pick a pile ");
+    alert(player + "'s turn. There are "+ rocks.length+" left.");
     var numRocks = prompt("Number Of rocks? 1-3");
-    if (rocks[pilePicked].includes('@')){
-        if(numRocks <= rocks[pilePicked].join(',').replace(/\,/g, "").length && numRocks <= 3 && numRocks >= 1){
-        for(var i = 0; i < numRocks; i++){
-            rocks[pilePicked].pop();
-            if(player === 'player 1'){
-            player = 'player 2';
-        }else{
-            player='player 1';
-        }
-        }
-    }else{
-        alert('bad selection');
-    }
+        if(numRocks <= rocks.length && numRocks <= 3 && numRocks >= 1){
+          for(var i = 0; i < numRocks; i++){
+              rocks.pop();
+              if(player === 'player 1'){
+              player = 'player 2';
+              }else{
+              player='player 1';
+            }}
     }else{
         alert('bad selection');
     }
