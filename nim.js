06014f5bcabdc82cbc3963fde838314e334c29b7
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-03 23:17:49 -0400

    start of NIM console game

---
+++ nim.js
@@ -0,0 +1,46 @@
+var player = 'player 1';
+var winner = false;
+rocks = [['@','@','@','@'],['@','@','@','@'],['@','@','@','@'],['@','@','@','@']];
+
+function printRocks() {
+    console.log(rocks[0]);
+    console.log(rocks[1]);
+    console.log(rocks[2]);
+    console.log(rocks[3]);
+}
+
+function checkPile(pile){
+    return rocks[pile];
+}
+
+function checkWinner(rocks){
+ if(rocks.join(',').replace(/\,/g, "") == '@'){
+    alert(player + 'wins');
+    winner = true;
+ }
+}
+
+function pickRocks(){
+    var pilePicked = prompt("Pick a pile ");
+    var numRocks = prompt("Number Of rocks? 1-3");
+    if (rocks[pilePicked].includes('@')){
+        if(numRocks <= rocks[pilePicked].join(',').replace(/\,/g, "").length){
+        for(var i = 0; i < numRocks; i++){
+            rocks[pilePicked].pop();
+            if(player === 'player 1'){
+            player = 'player 2';
+        }else{
+            player='player 1';
+        }
+        }
+    }
+    }else{
+        alert('bad selection');
+    }
+    checkWinner(rocks);
+    printRocks();
+}
+
+while(winner === false){
+    pickRocks();
+}
